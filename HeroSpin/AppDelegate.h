4f2f0802661860b1dfd9ac9ec677f29a282894e4
Peter Nagy <peter.x.nagy@gmail.com>
2016-04-05 20:13:46 +0200

    Initial single view application created

---
+++ HeroSpin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeroSpin
+//
+//  Created by Peter Nagy on 05/04/16.
+//  Copyright © 2016 Peter Nagy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
